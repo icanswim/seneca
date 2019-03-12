7384a4b5e2667c5acf933a9531680c465f95b386
Scott J Ward <wardsj@hotmail.com>
2019-03-12 00:29:39 -0700

    Update tf_pipe.py

--- tf_pipe.py
+++ tf_pipe.py
@@ -11,10 +11,12 @@ class TfPipe:
 	
 	def __init__(self, params, embedding=None):
 		
-		tf.set_random_seed(88)
+		#~ tf.set_random_seed(88)
 		self.params = params
 		self.params['embedding'] = embedding
 		self.estimator = None
+		self.model = None
+		self.sess = None
 			
 	def _input_func(self, X, y=None):
 		#dataset inputs must be roundly divisible by the batch_size
@@ -39,7 +41,7 @@ class TfPipe:
 			
 		dataset = tf.data.Dataset.from_tensor_slices(inputs)	
 		dataset = dataset.shuffle(X.shape[0])
-		#~ dataset = dataset.repeat()             
+		dataset = dataset.repeat(self.params['epochs'])             
 		dataset = dataset.batch(self.params['batch_size'])
 		
 		return dataset
@@ -98,8 +100,7 @@ class TfPipe:
 	def train_estimator(self, X_train, y_train):
 		
 		self.estimator = self._build_estimator()
-		self.estimator.train(input_fn=lambda:self._input_func(X_train, y_train), 
-								steps=self.params['training_steps'])
+		self.estimator.train(input_fn=lambda:self._input_func(X_train, y_train))
 		
 	def evaluate_estimator(self, X_test, y_test):
 		
@@ -115,7 +116,6 @@ class TfPipe:
 		#dataset inputs must be roundly divisible by the batch_size
 		dataset = {}
 		
-		dataset['num_classes'] = num_classes
 		dataset['X_train'] = tf.placeholder(X_train.dtype, X_train.shape)
 		dataset['y_train'] = tf.placeholder(y_train.dtype, y_train.shape)
 		dataset['X_test'] = tf.placeholder(X_test.dtype, X_test.shape)
@@ -124,11 +124,11 @@ class TfPipe:
 		train_dataset = tf.data.Dataset.from_tensor_slices(
 								(dataset['X_train'], dataset['y_train']))
 		train_dataset = train_dataset.shuffle(X_train.shape[0])              
-		train_dataset = train_dataset.batch(batch_size)
+		train_dataset = train_dataset.batch(self.params['batch_size'])
 		
 		test_dataset = tf.data.Dataset.from_tensor_slices(
 								(dataset['X_test'], dataset['y_test']))                
-		test_dataset = test_dataset.batch(batch_size)
+		test_dataset = test_dataset.batch(self.params['batch_size'])
 		
 		dataset['iterator'] = tf.data.Iterator.from_structure(
 					train_dataset.output_types, train_dataset.output_shapes)
@@ -141,26 +141,17 @@ class TfPipe:
 		
 		return dataset
 											
-	def train(self, X_train, y_train, X_test, y_test):
-		#TODO logic for classification vs regression
-		if classes:
-			self.num_classes = len(np.unique(y_train, axis=0))
-		
-		dataset = self._build_dataset(X_train, y_train, X_test, y_test, 
-										self.num_classes, batch_size)
+	def train_test(self, X_train, y_train, X_test, y_test):
+	
+		dataset = self._build_dataset(X_train, y_train, X_test, y_test)
 		
 		self.sess = tf.Session()
 		with self.sess.as_default():
 		
 			X, y = dataset['iterator'].get_next()
 			
-			self.model = load_model(model_name, 
-								input_tensor=X, 
-								output_shape=self.num_classes,
-								embedding=self.embedding,
-								batch_size=batch_size,
-								feature_shape=X_train.shape)
-			
+			self.model = load_model(self.params, X)
+											
 			predictions = tf.argmax(self.model.output, axis=1)
 			
 			if y_train.ndim == 1:
@@ -168,26 +159,24 @@ class TfPipe:
 			else:
 				labels = tf.argmax(y, axis=1)
 			
-			accuracy, accuracy_update = tf.metrics.accuracy(dataset['label'], 
+			accuracy, accuracy_op = tf.metrics.accuracy(dataset['label'], 
 												dataset['prediction'], 
 												name='accuracy')
 																		
-			train_step, loss = self._loss_func(y, self.model.output, loss_func)
+			train_op, loss = self._loss_func(y, self.model.output)
 				
 			self.sess.run(tf.global_variables_initializer())
 			
-			for e in range(epochs):
+			for e in range(self.params['epochs']):
 				self.sess.run(tf.local_variables_initializer())
 				self.sess.run(dataset['train_init'], feed_dict={
 					dataset['X_train']: X_train, dataset['y_train']: y_train})
-				self.model.training = True 
-				self.model.dropout = 0.0 #TODO dropout logic
-			
+				self.model.training = True
 				try:
 					while True:
-						_, loss_val = self.sess.run([train_step, loss])
+						_, loss_val = self.sess.run([train_op, loss])
 						label, prediction = self.sess.run([labels, predictions])
-						self.sess.run(accuracy_update, 
+						self.sess.run(accuracy_op, 
 										feed_dict={dataset['label']: label, 
 										dataset['prediction']: prediction})
 						
@@ -198,13 +187,11 @@ class TfPipe:
 				self.sess.run(tf.local_variables_initializer())
 				self.sess.run(dataset['test_init'], feed_dict={
 					dataset['X_test']: X_test, dataset['y_test']: y_test})
-				self.model.training = False
-				self.model.dropout = 0.0
-				
+				self.model.training = False				
 				try:
 					while True:
 						label, prediction = self.sess.run([labels, predictions])
-						self.sess.run(accuracy_update, 
+						self.sess.run(accuracy_op, 
 										feed_dict={dataset['label']: label, 
 										dataset['prediction']: prediction})
 			
